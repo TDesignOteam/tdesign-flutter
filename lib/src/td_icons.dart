333a1b8669a39139a94ac90c83d5ac85dfe04ba7
matthewyan <matthewyan@tencent.com>
2021-04-20 17:30:24 +0800

    feat: 添加td_icons类，引入TDesign图标库

---
+++ lib/src/td_icons.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:flutter/widgets.dart';
+
+@immutable
+class _TDIconsData extends IconData {
+  const _TDIconsData(int codePoint)
+      : super(
+          codePoint,
+          fontFamily: 'TDIcons',
+        );
+}
+
+@immutable
+class TDIcons {
+  TDIcons._();
+
+  
+  static const IconData add = _TDIconsData(0xe000);
+  static const IconData arrowDown = _TDIconsData(0xe001);
+  static const IconData arrowUp = _TDIconsData(0xe002);
+  static const IconData checkCircleFilled = _TDIconsData(0xe003);
+  static const IconData check = _TDIconsData(0xe004);
+  static const IconData chevronLeft = _TDIconsData(0xe005);
+  static const IconData chevronRight = _TDIconsData(0xe006);
+  static const IconData circle = _TDIconsData(0xe007);
+  static const IconData clearCircleFilled = _TDIconsData(0xe008);
+  static const IconData close = _TDIconsData(0xe009);
+  static const IconData errorCircleFilled = _TDIconsData(0xe00a);
+  static const IconData helpCircleFilled = _TDIconsData(0xe00b);
+  static const IconData loading = _TDIconsData(0xe00c);
+  static const IconData locationFilled = _TDIconsData(0xe00d);
+  static const IconData refresh = _TDIconsData(0xe00e);
+  static const IconData remove = _TDIconsData(0xe00f);
+  static const IconData search = _TDIconsData(0xe010);
+  static const IconData soundFilled = _TDIconsData(0xe011);
+  static const IconData starFilled = _TDIconsData(0xe012);
+  static const IconData timeFilled = _TDIconsData(0xe013);
+  static const IconData userFilled = _TDIconsData(0xe014);
+  static const IconData warning = _TDIconsData(0xe015);
+}
---
+++ test/td_icons_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdesign_flutter/tdesign_flutter.dart';
+
+void main() {
+  group('td_icons', () {
+    test('icons valid', () {
+      expect(TDIcons.add != null, true);
+    });
+  });
+}
