2373641f4b8c9d495aaea9ced2f5d90551b46fd8
thomasfan <thomasfan@tencent.com>
2021-06-24 09:48:48 +0800

    fix(toast): 修改显示时间和位置参数的默认值；对齐交互稿调整测试页面按钮风格及排版。

--- example/lib/pages/toast_page.dart
+++ example/lib/pages/toast_page.dart
@@ -18,104 +18,56 @@ class _ToastPageState extends State<ToastPage> {
 
   @override
   Widget build(BuildContext context) {
-    Toast.init();
     return ExamplePage(child: _widget(), title: 'Toast');
+  }
+
+  Widget _outlinedButton(
+    String btnTitle,
+    VoidCallback callback,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0, left: 16.0, right: 16.0),
+      child: OutlinedButton(
+          child: Text(
+            btnTitle,
+            style: TextStyle(
+                color: Colors.black.withOpacity(0.9),
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                height: 1.5),
+          ),
+          style: ButtonStyle(
+              fixedSize: MaterialStateProperty.resolveWith(
+                  (states) => Size.fromHeight(48))),
+          onPressed: callback),
+    );
   }
 
   Widget _widget() {
     return ListView(children: [
       PageUtil.buildSubTitle('文本轻提示'),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('短文本轻提示'), onPressed: () => Toast.toast('轻提示内容')),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('长文本轻提示'),
-            onPressed: () =>
-                Toast.toast('这是一条长文字提示，超过一定字数就会换行。这是一条长文字提示，超过一定字数就会换行')),
-      ),
+      _outlinedButton('短文本轻提示', () => Toast.toast('轻提示内容')),
+      _outlinedButton('长文本轻提示',
+          () => Toast.toast('这是一条长文字提示，超过一定字数就会换行。这是一条长文字提示，超过一定字数就会换行')),
       PageUtil.buildSubTitle('图标轻提示'),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child:
-            ElevatedButton(child: Text('成功'), onPressed: () => Toast.success()),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(child: Text('警告'), onPressed: () => Toast.fail()),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child:
-            ElevatedButton(child: Text('加载'), onPressed: () => Toast.loading()),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child:
-            ElevatedButton(child: Text('自定义图标'), onPressed: () => Toast.info()),
-      ),
+      _outlinedButton('成功', () => Toast.success()),
+      _outlinedButton('警告', () => Toast.fail()),
+      _outlinedButton('加载', () => Toast.loading()),
+      _outlinedButton('自定义图标', () => Toast.info()),
       PageUtil.buildSubTitle('图标加文本轻提示'),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('成功'), onPressed: () => Toast.success(message: '成功文案')),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('警告'), onPressed: () => Toast.fail(message: '失败文案')),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('加载'), onPressed: () => Toast.loading(message: '加载')),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('自定义图标'),
-            onPressed: () => Toast.info(message: '自定义图标')),
-      ),
+      _outlinedButton('成功', () => Toast.success(message: '成功文案')),
+      _outlinedButton('警告', () => Toast.fail(message: '失败文案')),
+      _outlinedButton('加载', () => Toast.loading(message: '加载')),
+      _outlinedButton('自定义图标', () => Toast.info(message: '自定义图标')),
       PageUtil.buildSubTitle('提示位置'),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('顶部Top'),
-            onPressed: () => Toast.success(toastPosition: ToastPosition.top)),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('中间Middle'),
-            onPressed: () =>
-                Toast.success(toastPosition: ToastPosition.middle)),
-      ),
-      Divider(height: 0),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('底部Bottom'),
-            onPressed: () =>
-                Toast.success(toastPosition: ToastPosition.bottom)),
-      ),
+      _outlinedButton(
+          '顶部Top', () => Toast.success(toastPosition: ToastPosition.top)),
+      _outlinedButton(
+          '中间Middle', () => Toast.success(toastPosition: ToastPosition.middle)),
+      _outlinedButton(
+          '底部Bottom', () => Toast.success(toastPosition: ToastPosition.bottom)),
       PageUtil.buildSubTitle('z遮罩'),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            child: Text('带背景遮罩'),
-            onPressed: () => Toast.toast('轻提示内容', showOverlay: true)),
-      ),
+      _outlinedButton('带背景遮罩', () => Toast.toast('轻提示内容', showOverlay: true)),
     ]);
   }
 }
