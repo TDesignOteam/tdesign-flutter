d7033b343db77eed0aa2927115339e52b140d02c
jackiexing <jackiexing@tencent.com>
2021-07-23 21:09:40 +0800

    feat(noticebar.dart): 实现了一个新的组件NoticeBar

--- lib/tdesign.dart
+++ lib/tdesign.dart
@@ -3,24 +3,17 @@
 library tdesign;
 
 export 'src/badge.dart';
-
-export 'src/checkbox.dart';
-export 'src/dialog.dart';
-
-export 'src/message.dart';
-export 'src/message.dart';
 export 'src/picker.dart';
-export 'src/radio.dart';
 export 'src/rate.dart';
 export 'src/tags.dart';
 export 'src/icons.dart';
 export 'src/toast.dart';
-
 export 'src/message.dart';
 export 'src/dialog.dart';
 export 'src/popup.dart';
 export 'src/checkbox.dart';
 export 'src/radio.dart';
 export 'src/switch.dart';
+export 'src/noticebar.dart';
 export 'theme/td_colors.dart';
 export 'theme/td_theme.dart';
