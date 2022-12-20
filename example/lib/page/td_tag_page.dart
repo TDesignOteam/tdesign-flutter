11a7bc89766f130807a14d8ae30472bbde66115b
zflyluo <zflyluo@tencent.com>
2022-12-20 15:43:42 +0800

    添加tag演示代码

--- example/lib/page/td_tag_page.dart
+++ example/lib/page/td_tag_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tdesign_flutter/td_export.dart';
 import '../../base/example_widget.dart';
+import '../annotation/demo.dart';
 
 class TDTagPage extends StatelessWidget {
   const TDTagPage({Key? key}) : super(key: key);
@@ -9,64 +10,13 @@ class TDTagPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ExamplePage(title: '标签 Tag',
         // padding: EdgeInsets.zero,
+        exampleCodeGroup: 'tag',
         children: [
         ExampleModule(title: '默认',
         children: [
           ExampleItem(
               desc: '展示标签',
-              builder: (context) {
-                return Wrap(
-                  spacing: 16,
-                  runSpacing: 16,
-                  children: [
-                    const TDTag(
-                      '标签',
-                    ),
-                    const TDTag('成功',
-                        style: RoundRectTagStyle(type: TDTagType.success)),
-                    const TDTag('警告',
-                        style: RoundRectTagStyle(type: TDTagType.warning)),
-                    const TDTag('危险',
-                        style: RoundRectTagStyle(type: TDTagType.error)),
-                    const TDTag('信息',
-                        style: RoundRectTagStyle(type: TDTagType.message)),
-                    const TDTag('浅色', style: RoundRectTagStyle(isLight: true)),
-                    const TDTag('描边', style: WireframeRoundRectTagStyle()),
-                    const TDTag('浅色描边',
-                        style: WireframeRoundRectTagStyle(isLight: true)),
-                    const TDTag(
-                      'English',
-                    ),
-                    const TDTag('English',
-                        style: WireframeRoundRectTagStyle(isLight: true)),
-                    const TDTag(
-                      'ABC',
-                    ),
-                    const TDTag('ABC',
-                        style: WireframeRoundRectTagStyle(isLight: true)),
-                    const TDTag(
-                      '中English混合',
-                    ),
-                    const TDTag('中English混合',
-                        style: WireframeRoundRectTagStyle(isLight: true)),
-                    TDTag(
-                      '圆角',
-                      style: CircleRectTagStyle(),
-                    ),
-                    TDTag(
-                      '半圆',
-                      style: SemicircleRectTagStyle(),
-                    ),
-                    TDTag(
-                      '标签',
-                      needCloseIcon: true,
-                      onCloseTap: () {
-                        TDToast.showText('点击了关闭图标', context: context);
-                      },
-                    ),
-                  ],
-                );
-              }),
+              builder: _buildShowTag),
           ExampleItem(
               desc: '点击控件',
               builder: (context) {
@@ -227,5 +177,60 @@ class TDTagPage extends StatelessWidget {
                 );
               }),
         ])]);
+  }
+
+  @Demo(group: 'tag')
+  Widget _buildShowTag(BuildContext context) {
+    return Wrap(
+      spacing: 16,
+      runSpacing: 16,
+      children: [
+        const TDTag(
+          '标签',
+        ),
+        const TDTag('成功',
+            style: RoundRectTagStyle(type: TDTagType.success)),
+        const TDTag('警告',
+            style: RoundRectTagStyle(type: TDTagType.warning)),
+        const TDTag('危险',
+            style: RoundRectTagStyle(type: TDTagType.error)),
+        const TDTag('信息',
+            style: RoundRectTagStyle(type: TDTagType.message)),
+        const TDTag('浅色', style: RoundRectTagStyle(isLight: true)),
+        const TDTag('描边', style: WireframeRoundRectTagStyle()),
+        const TDTag('浅色描边',
+            style: WireframeRoundRectTagStyle(isLight: true)),
+        const TDTag(
+          'English',
+        ),
+        const TDTag('English',
+            style: WireframeRoundRectTagStyle(isLight: true)),
+        const TDTag(
+          'ABC',
+        ),
+        const TDTag('ABC',
+            style: WireframeRoundRectTagStyle(isLight: true)),
+        const TDTag(
+          '中English混合',
+        ),
+        const TDTag('中English混合',
+            style: WireframeRoundRectTagStyle(isLight: true)),
+        TDTag(
+          '圆角',
+          style: CircleRectTagStyle(),
+        ),
+        TDTag(
+          '半圆',
+          style: SemicircleRectTagStyle(),
+        ),
+        TDTag(
+          '标签',
+          needCloseIcon: true,
+          onCloseTap: () {
+            TDToast.showText('点击了关闭图标', context: context);
+          },
+        ),
+      ],
+    );
   }
 }
